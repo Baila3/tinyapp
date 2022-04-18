4f7dc904a0ab5b143a160b79452f2b86dec33a6d
Baila3 <bailaly16@gmail.com>
2022-04-18 17:22:55 -0400

    new function

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req,res) => {
   res.send("Hello!");
 });
@@ -31,7 +34,22 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL  };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
+
+function generateRandomString() {
+  return  Math.random().toString(36).slice(2,8);
+}
+
+
